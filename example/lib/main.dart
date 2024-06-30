ce368a554ba580b82cd5e512987c3c3296006abc
Your Name <you@example.com>
2024-06-30 13:33:57 +0500

    updated

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:voice_message_recorder/mySize.dart';
 import 'package:voice_message_recorder/phone_textfield/phone_number_field.dart';
+import 'package:voice_message_recorder/voice_message_player/src/voice_controller.dart';
+import 'package:voice_message_recorder/voice_message_player/src/voice_message_view.dart';
 import 'package:voice_message_recorder/voice_message_recorder/audio_encoder_type.dart';
 import 'package:voice_message_recorder/voice_message_recorder/screen/voice_message_recorder.dart';
 
@@ -43,7 +45,77 @@ class Home extends StatelessWidget {
                   MaterialPageRoute(builder: (context) => SingleChatScreen()));
             },
             child: Text('SingleChatScreen')),
+        ElevatedButton(
+            onPressed: () {
+              Navigator.of(context)
+                  .push(MaterialPageRoute(builder: (context) => player()));
+            },
+            child: Text('player')),
       ],
+    );
+  }
+}
+
+class player extends StatelessWidget {
+  player({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      backgroundColor: Colors.grey.shade200,
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              VoiceMessagePlayer(
+                controller: VoiceController(
+                  audioSrc:
+                      'https://dl.solahangs.com/Music/1403/02/H/128/Hiphopologist%20-%20Shakkak%20%28128%29.mp3',
+                  maxDuration: const Duration(seconds: 10),
+                  isFile: false,
+                  onComplete: () {
+                    /// do something on complete
+                  },
+                  onPause: () {
+                    /// do something on pause
+                  },
+                  onPlaying: () {
+                    /// do something on playing
+                  },
+                  onError: (err) {
+                    /// do somethin on error
+                  },
+                ),
+                innerPadding: 12,
+                cornerRadius: 20,
+              ),
+              VoiceMessagePlayer(
+                controller: VoiceController(
+                  audioSrc:
+                      'https://cdn.pixabay.com/download/audio/2022/11/16/audio_a2b0a45199.mp3?filename=6-islamic-background-sounds-alfa-relaxing-music-126060.mp3',
+                  maxDuration: const Duration(seconds: 10),
+                  isFile: false,
+                  onComplete: () {
+                    /// do something on complete
+                  },
+                  onPause: () {
+                    /// do something on pause
+                  },
+                  onPlaying: () {
+                    /// do something on playing
+                  },
+                  onError: (err) {
+                    /// do somethin on error
+                  },
+                ),
+                innerPadding: 12,
+                cornerRadius: 20,
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,20 +5,26 @@
 import FlutterMacOS
 import Foundation
 
+import audio_session
 import audioplayers_darwin
 import emoji_picker_flutter
+import just_audio
 import path_provider_foundation
 import record_macos
 import shared_preferences_foundation
+import sqflite
 import video_player_avfoundation
 import voice_message_recorder
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
   AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
   EmojiPickerFlutterPlugin.register(with: registry.registrar(forPlugin: "EmojiPickerFlutterPlugin"))
+  JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   RecordMacosPlugin.register(with: registry.registrar(forPlugin: "RecordMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
   VoiceMessageRecorderPlugin.register(with: registry.registrar(forPlugin: "VoiceMessageRecorderPlugin"))
 }
